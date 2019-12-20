0f32a73a991c70338594ff6509f96ff6e1e49adf
tianyu5177 <zhangtianyu5177@sina.com>
2019-12-20 17:23:32 +0800

    更新

--- 06_src_redux_modular/redux/reducers/index.js
+++ 06_src_redux_modular/redux/reducers/index.js
@@ -3,7 +3,6 @@ import CounterReducer from './counter_reducer'
 import PersonReducer from './person_reducer'
 import {combineReducers} from 'redux'
 
-
 export default combineReducers({
 	count:CounterReducer,
 	persons:PersonReducer
