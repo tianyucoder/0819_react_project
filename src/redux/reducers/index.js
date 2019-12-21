e33a80d5b83b5f69311b663b49e087fcb970e9a5
tianyu5177 <zhangtianyu5177@sina.com>
2019-12-21 17:07:40 +0800

    更新

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,19 @@
-import {combineReducers} from 'redux'
-import LoginReducer from './login_reducer'
+import {combineReducers} from 'redux' //引入combineReducers用于合并多个reducer
+import LoginReducer from './login_reducer' //引入login_reducer
 
+//经过combineReducers，汇总一个总状态交给redux
 export default combineReducers({
 	userInfo:LoginReducer
 })
+
+/* 
+	redux中保存的总状态如下：
+		{
+			userInfo：{
+				user:xxxxxx
+				token:xxxxxx
+				isLogin:xxxxx
+			}
+		}
+
+*/
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import {createStore,applyMiddleware} from 'redux'
-import reducer from './reducers'
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import {createStore,applyMiddleware} from 'redux' //引入核心的redux
+import reducer from './reducers' //引入合并后的reducer
+import thunk from 'redux-thunk' //引入thunk，用于异步编码
+import {composeWithDevTools} from 'redux-devtools-extension'//支持开发者工具
 
 export default createStore(reducer,composeWithDevTools(applyMiddleware(thunk)))
