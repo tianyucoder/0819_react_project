7fcfdab6b21ef75e24a8a2003b3e8a156336bc43
tianyu5177 <zhangtianyu5177@sina.com>
2019-12-22 08:38:19 +0800

    登录&自动登录

---
+++ 03_day03_ajax/pages/admin/admin.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {deleteUserInfo} from '../../redux/actions/login_action'
+import {Redirect} from 'react-router-dom'
+
+class Admin extends Component {
+
+	logout = ()=>{
+		//删除redux、localStorage中保存的用户数据
+		this.props.deleteUserInfo()
+	}
+
+	render() {
+		if(!this.props.userInfo.isLogin){
+			//进入此判断，意味着：用户已经登录，但是还要看login，不允许，强制跳转到admin
+			//this.props.history.replace('/admin')
+			return <Redirect to="/login"/>
+		}
+		return (
+			<div style={{fontSize:"25px"}}>
+				hello,{this.props.userInfo.user.username}
+				<button onClick={this.logout}>退出登录</button>
+			</div>
+		)
+	}
+}
+
+export default connect(
+	state => ({userInfo:state.userInfo}),
+	{deleteUserInfo}
+)(Admin)
---
+++ 03_day03_ajax/pages/login/login.jsx
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import { Form, Icon, Input, Button,message} from 'antd';
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
+import {saveUserInfo} from '../../redux/actions/login_action'
+import {reqLogin} from '../../api'
+import logo from './images/logo.png'
+import './css/login.less'
+const {Item} = Form
+
+class Login extends Component {
+	//响应表单的提交
+	handleSubmit = (event)=>{
+		event.preventDefault()
+		this.props.form.validateFields(async(err, values) => {
+			//进行表单最后一步统一验证
+      if (!err) {
+				let loginResult = await reqLogin(values)
+				const {status,data,msg} = loginResult
+				if(status === 0){
+					console.log(data);
+					message.success('登录成功',1)
+					this.props.history.push('/admin')
+					//此处把data交给redux管理
+					this.props.saveUserInfo(data)
+				}else{
+					message.warning(msg,1)
+				}
+			}
+    });
+	}
+
+	//自定义密码的验证器
+	pwdValidator = (rule, value, callback)=>{
+		if(!value){
+			callback('密码必须输入')
+		}else if(value.length < 4){
+			callback('密码必须大于等于4位')
+		}else if(value.length > 12){
+			callback('密码必须小于等于12位')
+		}else if(!(/^\w+$/).test(value)){
+			callback('密码必须是英文、数字或下划线组成')
+		}
+		callback()
+	}
+
+	render() {
+		if(this.props.userInfo.isLogin){
+			//进入此判断，意味着：用户已经登录，但是还要看login，不允许，强制跳转到admin
+			//this.props.history.replace('/admin')
+			return <Redirect to="/admin"/>
+		}
+		const { getFieldDecorator } = this.props.form;
+		return (
+			<div id="login">
+				<div className="header">
+					<img src={logo} alt="logo"/>
+					<h1>商品管理系统</h1>
+				</div>
+				<div className="content">
+					<h1>用户登录</h1>
+					<Form onSubmit={this.handleSubmit} className="login-form">
+						<Item>
+							{/* 用户名/密码的的合法性要求
+									1). 必须输入
+									2). 必须大于等于4位
+									3). 必须小于等于12位
+									4). 必须是英文、数字或下划线组成 */}
+							{getFieldDecorator('username', {
+								rules: [
+									{required: true, message: '用户名必须输入'}, //限制该输入域必须输入
+									{min: 4, message: '用户名必须大于等于4位'}, //限制该输入域必须输入
+									{max: 12, message: '用户名必须小于等于12位'}, //限制该输入域必须输入
+									{pattern: /^\w+$/, message: '用户名必须是英文、数字或下划线组成'}, //限制该输入域必须输入
+								],
+							})(
+								<Input
+									prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+									placeholder="用户名"
+								/>
+							)}
+						</Item>
+						<Item>
+							{getFieldDecorator('password', {
+									rules: [
+										{validator:this.pwdValidator}, 
+									],
+								})(
+									<Input
+										prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+										type="password"
+										placeholder="密码"
+									/>
+								)}
+						</Item>
+						<Item>
+							<Button type="primary" htmlType="submit" className="login-form-button">
+								登录
+							</Button>
+						</Item>
+					</Form>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default connect(
+	state => ({userInfo:state.userInfo}),
+	{saveUserInfo}
+)(Form.create()(Login))
