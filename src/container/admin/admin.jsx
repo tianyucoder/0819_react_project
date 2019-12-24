5d29e152052ed5217c4fe475d21050ceeb7d65ad
tianyu5177 <zhangtianyu5177@sina.com>
2019-12-24 10:51:49 +0800

    自动生成菜单

--- src/container/admin/admin.jsx
+++ src/container/admin/admin.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {Layout} from 'antd';
+import {Switch,Route} from 'react-router-dom'
 import checkLogin from '../check_login/check_login'
 import {deleteUserInfo} from '../../redux/actions/login_action'
 import Header from './header/header'
 import LeftNav from './left_nav/left_nav'
+import Home from '../../components/home/home'
+import Category from '../category/category'
+import Product from '../product/product'
+import User from '../user/user'
+import Role from '../role/role'
+import Bar from '../bar/bar'
+import Line from '../line/line'
+import Pie from '../pie/pie'
 import './css/admin.less'
 const { Footer, Sider, Content} = Layout;
 
@@ -24,8 +33,19 @@ class Admin extends Component {
 				</Sider>
 				<Layout>
 					<Header/>
-					<Content>Content</Content>
-					<Footer>Footer</Footer>
+					<Content className="contnet">
+						<Switch>
+							<Route path="/admin/home" component={Home}/>
+							<Route path="/admin/prod_about/category" component={Category}/>
+							<Route path="/admin/prod_about/product" component={Product}/>
+							<Route path="/admin/user" component={User}/>
+							<Route path="/admin/role" component={Role}/>
+							<Route path="/admin/charts/bar" component={Bar}/>
+							<Route path="/admin/charts/line" component={Line}/>
+							<Route path="/admin/charts/pie" component={Pie}/>
+						</Switch>
+					</Content>
+					<Footer className="footer">推荐使用谷歌浏览器，获取最佳用户体验</Footer>
 				</Layout>
 			</Layout>
 		)
